b1c749a6c4a5b4749a6db44337a1ba95938afd29
matheodrd <matheodurand14@gmail.com>
2025-05-02 22:40:59 +0200

    feat: add a go routine with a semaphore to handle pub/sub messages

--- internal/subscriber/subscriber.go
+++ internal/subscriber/subscriber.go
@@ -8,18 +8,20 @@ import (
 )
 
 type Subscriber struct {
-	config *config.Config
-	logger *slog.Logger
-	client *redis.Client
-	topic  string
+	config        *config.Config
+	logger        *slog.Logger
+	client        *redis.Client
+	topic         string
+	maxConcurrent int
 }
 
-func NewSubscriber(config *config.Config, logger *slog.Logger, client *redis.Client, topic string) *Subscriber {
+func NewSubscriber(config *config.Config, logger *slog.Logger, client *redis.Client, topic string, maxConcurrent int) *Subscriber {
 	return &Subscriber{
 		config,
 		logger,
 		client,
 		topic,
+		maxConcurrent,
 	}
 }
 
@@ -34,6 +36,11 @@ func (s *Subscriber) Start(ctx context.Context) error {
 
 	msgCh := pubsub.Channel()
 
+	// Improvised semaphore to avoid too many go routines (handleMessage).
+	// A better but more complex solution would be a worker pool. But franchement OSEF.
+	// https://medium.com/@deckarep/gos-extended-concurrency-semaphores-part-1-5eeabfa351ce
+	// https://gobyexample.com/worker-pools
+	sem := make(chan struct{}, s.maxConcurrent)
 	for {
 		select {
 		case msg, ok := <-msgCh:
@@ -41,8 +48,16 @@ func (s *Subscriber) Start(ctx context.Context) error {
 				s.logger.Warn("pubsub channel closed by Redis")
 				return nil
 			}
-			if err := s.handleMessage(ctx, msg); err != nil {
-				s.logger.Error("error handling message", "error", err)
+			select {
+			case sem <- struct{}{}:
+				go func(m *redis.Message) {
+					defer func() { <-sem }()
+					if err := s.handleMessage(ctx, m); err != nil {
+						s.logger.Error("error handling message", "error", err)
+					}
+				}(msg)
+			case <-ctx.Done():
+				return nil
 			}
 		case <-ctx.Done():
 			s.logger.Info("shutting down Redis subscriber")
