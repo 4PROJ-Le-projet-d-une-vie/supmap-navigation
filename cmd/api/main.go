e7c82c2df8cbad41d564a4627c76a9102bc00a39
matheodrd <matheodurand14@gmail.com>
2025-05-02 19:25:56 +0200

    feat: add cli to start API server

---
+++ cmd/api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+	"supmap-navigation/internal/api"
+	"supmap-navigation/internal/config"
+	"syscall"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	conf, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	jsonHandler := slog.NewJSONHandler(os.Stdout, nil)
+	logger := slog.New(jsonHandler)
+
+	server := api.NewServer(conf, logger)
+	if err := server.Start(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
